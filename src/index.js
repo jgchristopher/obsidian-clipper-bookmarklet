c849b87f46888749977bf13a289b84b54004488b
John Christopher <jgchristopher@gmail.com>
2022-10-07 13:56:49 -0500

    start using advanced-uri plugin and clean up code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import TurndownService from 'turndown';
 
 (() => {
-  /* Optional vault name */
-  const vault = "";
-  const daily_notes = "daily_notes/";
-  /* Optional tags  */
-  const tags = "#bookmark-clipper";
+  const markdownService = new TurndownService({
+    headingStyle: 'atx',
+    hr: '---',
+    bulletListMarker: '-',
+    codeBlockStyle: 'fenced',
+    emDelimiter: '*',
+  });
 
   function convertDate(date) {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    return `${year}-${month}-${day}`;
+    let dateString = '';
+
+    function padLeftZero(value) {
+      console.log(value)
+      let arr = value.split('');
+      return (arr[1] ? value : "0" + arr[0])
+    }
+    
+    if (date) {
+      let year = date.getFullYear().toString();
+      let mm = padLeftZero((date.getMonth() + 1).toString());
+      let dd = padLeftZero(date.getDate().toString());
+      dateString = `${year}-${mm}-${dd}`;
+    }
+    return dateString;
   }
 
   function getSelectionHtml() {
@@ -29,19 +42,23 @@ import TurndownService from 'turndown';
     return html;
   }
 
-  const selection = getSelectionHtml();
-  const markdownify = selection ? selection : "";
-  const vaultName = vault ? `&vault=${encodeURIComponent(vault)}` : '';
-  const today = convertDate(new Date());
-  const sendToObsidian = (content) => {
-    document.location.href = `obsidian://new?file=${encodeURIComponent(daily_notes + today)}&content=${encodeURIComponent(content)}&append=true${vaultName}`;
+  function sendToObsidian(content, heading, vault) {
+    let data = encodeURIComponent(content);
+    let header = encodeURIComponent(heading);
+
+    // This uses the built in obsidian url support
+    // let defaultUrl =  `obsidian://new?file=${encodeURIComponent(daily_notes + convertDate(new Date()))}&content=${encodeURIComponent(content)}&append=true&vault=${vaultName}`;
+    //document.location.href = defaultUrl;
+
+    // This requires the advanced-uri plugin to be installed
+    let url = `obsidian://advanced-uri?vault=${vault}&daily=true&heading=${header}&data=${data}&mode=prepend`;
+    document.location.href = url;
   }
-  const markdownBody = new TurndownService({
-    headingStyle: 'atx',
-    hr: '---',
-    bulletListMarker: '-',
-    codeBlockStyle: 'fenced',
-    emDelimiter: '*',
-  }).turndown(markdownify);
-  sendToObsidian(`- [${document.title}](${document.URL}) ${tags}\n${markdownBody}\n\n---\n\n`);
+
+  const vaultName = encodeURIComponent("Personal");
+  const daily_notes = "daily_notes/";
+  const heading = "Daily Log";
+  const tags = "#bookmark-clipper";
+  const content = `- [${document.title}](${document.URL}) ${tags}\n${markdownService.turndown(getSelectionHtml())}\n\n---\n\n`
+  sendToObsidian(content, heading, vaultName);
 })();
